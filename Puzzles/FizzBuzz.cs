7a003b48a31895b4fd5ccdb0bd42a4b23ebfd2b9
spavkov <spavkov@gmail.com>
2019-03-31 20:25:17 +0200

    Initial commit

---
+++ Puzzles/FizzBuzz.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ProgrammingExcercises1.Puzzles
+{
+    public class FizzBuzz
+    {
+        /* "Write a program that prints the numbers from 1 to 100. 
+        But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”. 
+        For numbers which are multiples of both three and five print “FizzBuzz”."
+
+        Example oputput for numbers from 1 to 5 would be: 
+        "1 2 Fizz 4 Buzz"
+
+        Program needs to print output for numbers 1 to 100
+        */
+
+        public static string Solve()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
