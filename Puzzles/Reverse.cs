fdad1b08a9d330aae6d0710753bbb025e82cf358
spavkov <spavkov@gmail.com>
2019-04-03 21:04:09 +0200

    Adding reverse string test

---
+++ Puzzles/Reverse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ProgrammingExcercises1.Puzzles
+{
+    public class Reverse
+    {
+        /* "Write a program that prints the string backwards. 
+
+        Example oputput for string "iknowhowtocode" should be "edocotwohwonki"
+        */
+
+        public static string Solve(string input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Tests/ReverseTests.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProgrammingExcercises1.Puzzles;
+
+namespace ProgrammingExcercises1.Tests
+{
+    [TestClass]
+    public class ReverseTests
+    {
+        [TestMethod]
+        public void ShouldReturnProperTest()
+        {
+            Assert.AreEqual(Reverse.Solve("iknowhowtocode"),
+                "edocotwohwonki");
+        }
+    }
+}
