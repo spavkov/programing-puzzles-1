fb682794803cc5225168579141386ff785b332ca
Slobodan Pavkov <slpavkov@microsoft.com>
2019-04-09 14:56:07 +0200

    adding SumOfDigits

---
+++ Puzzles/SumOfDigits.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ProgrammingExcercises1.Puzzles
+{
+    public class SumOfDigits
+    {
+        /* "Write an implementation of method Solve in this class that returns the sum of individual digits of number passed as parameter. 
+
+        Example oputput for "123" should be integer 6
+        Example oputput for "45" should be integer 9
+        Example oputput for "1231" should be integer 7
+        Example oputput for "1" should be integer 1
+        Example oputput for "00" should be integer 0
+        */
+
+        public static int Solve(int number)
+        {
+            // ne zaboravi da uklonis ovaj red ispod, jer inace kompajler ne gleda kod ispod te linije, kao da ne postoji
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Tests/SumOfDigitsTests.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProgrammingExcercises1.Puzzles;
+
+namespace ProgrammingExcercises1.Tests
+{
+    [TestClass]
+    public class SumOfDigitsTests
+    {
+        [TestMethod]
+        public void ShouldReturnProperTest1()
+        {
+            Assert.AreEqual(SumOfDigits.Solve(123), 6);
+        }
+
+        [TestMethod]
+        public void ShouldReturnProperTest2()
+        {
+            Assert.AreEqual(SumOfDigits.Solve(1234), 10);
+        }
+
+        [TestMethod]
+        public void ShouldReturnProperTest3()
+        {
+            Assert.AreEqual(SumOfDigits.Solve(00000), 0);
+        }
+
+        [TestMethod]
+        public void ShouldReturnProperTest4()
+        {
+            Assert.AreEqual(SumOfDigits.Solve(1), 1);
+        }
+
+        [TestMethod]
+        public void ShouldReturnProperTest5()
+        {
+            Assert.AreEqual(SumOfDigits.Solve(1111111111), 10);
+        }
+    }
+}
